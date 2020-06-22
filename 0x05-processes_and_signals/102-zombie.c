ece1750d8534012c4cfe00e3e32bccbe7c88a8e8
medcharfi96 <1389@holbertonschool.com>
2020-06-22 23:16:46 +0200

    add

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+/**
+* infinite_while - loop without end
+*
+*
+* Return: int(mean true)
+*/
+
+int infinite_while(void)
+{
+while (1)
+{
+	sleep(1);
+}
+return (0);
+}
+
+/**
+* main - main function
+*
+*
+* Return: int (mean true)
+*/
+int main(void)
+{
+int count = 0;
+
+for (count; count < 5; count++)
+{
+	if (fork() >= 0)
+{
+	printf("Zombie process created, PID: %d\n", getpid());
+}
+else
+return (0);
+}
+infinite_while();
+return (0);
+}
