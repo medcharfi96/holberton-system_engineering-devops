46f58d223e34f286e03c0464b1cddd662f3c2c73
medcharfi96 <1389@holbertonschool.com>
2020-09-15 00:29:35 +0200

    add

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+task0
+"""
+
+
+from sys import argv
+import json
+import requests
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/users/"
+    url102 = "https://jsonplaceholder.typicode.com/todos/"
+    employe = int(argv[1])
+    user = requests.get(url + "{}".format(int(argv[1])))
+    nom = user.json().get('name')
+    usnom = user.json().get('username')
+    kraya = requests.get(url102, params={"userId": argv[1]})
+    matiere = kraya.json()
+    mat_thazet = 0
+    mat_gen = 0
+    tabledemat = []
+    tabledemat_2 = []
+    for z in matiere:
+        mini_cel = {}
+        mat_gen += 1
+        mini_cel['task'] = z.get("title")
+        mini_cel['completed'] = z.get("completed")
+        mini_cel['username'] = usnom
+        tabledemat_2.append(mini_cel)
+        if z.get('completed'):
+            mat_thazet += 1
+            tabledemat.append(z.get("title"))
+    print('Employee {} is done with tasks({}/{}):'
+          .format(nom, mat_thazet, mat_gen))
+    print('\n')
+    for i in range(0, len(tabledemat)):
+        print("\t" + tabledemat[i] + '\n')
+    nom_fichier = argv[1] + '.json'
+    with open(nom_fichier, 'w') as fichie:
+        json.dump(tabledemat_2, fichie)
