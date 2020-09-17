a3a943ba90359720679c9eae37d5d386144487d1
medcharfi96 <1389@holbertonschool.com>
2020-09-17 14:21:27 +0200

    addd

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+function that give the number of user iven not active
+"""
+
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    utlisateur = {
+        "User-Agent": "User-Agent"
+    }
+    URLadress = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    request = requests.get(URLadress, headers=utlisateur)
+    try:
+        return request.json().get('data').get('subreddit_subscribers')
+    except:
+        return(0)
