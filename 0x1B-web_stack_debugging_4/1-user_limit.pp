740319253e7150cefd49826e8956024e836fc405
medcharfi96 <1389@holbertonschool.com>
2020-11-02 23:41:59 +0100

    add

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,7 @@
 #task adv
 exec { 'user':
-command  => "sudo /bin/sed -i 's/nofile 6895/g' /etc/security/limits.conf",
+command  => "sudo sed -i 's/holberton hard nofile 5/holberton hard nofile 6003/g' /etc/security/limits.conf
+sudo sed -i 's/holberton soft nofile 4/holberton soft nofile 6003/g' /etc/security/limits.conf",
 path     => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
 provider => shell,
 }
